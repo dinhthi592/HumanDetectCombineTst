8c4e7a470f198aa222f2f157e9a19130d6dc4337
tlong <tlong@P1114>
2020-05-21 10:02:32 +0900

    first commit

---
+++ TestcaseSimple/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestcaseSimple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
